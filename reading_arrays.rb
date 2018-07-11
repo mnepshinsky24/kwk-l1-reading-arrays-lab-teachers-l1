bc0ec4b189ba0ad5916a3052fd620e4a4759aa24
Megan Nepshinsky <mnepshinsky@gmail.com>
2018-07-11 01:36:05 +0000

    automatically backed up by learn

--- reading_arrays.rb
+++ reading_arrays.rb
@@ -38,7 +38,7 @@ end
 last_student_by_method
 
 def first_second_and_third_students
-puts $student_names[0..2]  # Write a solution that returns the first, second and third students
+puts STUDENT_NAMES[0..2]  # Write a solution that returns the first, second and third students
 end
 
 first_second_and_third_students
